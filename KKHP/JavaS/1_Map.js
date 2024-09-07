8e6f44cfafa1cb56491d65becc01a9e11cd8e4a3
Tha93-HDMI <takumi_hidema@icloud.com>
2024-09-08 08:05:22 +0900

    Update 1_Map.js

--- KKHP/JavaS/1_Map.js
+++ KKHP/JavaS/1_Map.js
@@ -3,7 +3,12 @@ var Floor = 0 ;
 var Blocks = [];
 var BlockP = [];
 var MapList = [];
+var PlaceIn = [];
+var PlaceP = "";
+var InfoPlace = "";
+var NowNum = -1;
 for ( i=0 ; i < 52 ; i++ ) {
+    PlaceIn.push([])
     Blocks.push(document.getElementById("B"+String( i )))
     BlockP.push("<span>")
 };
@@ -46,23 +51,57 @@ function FSW() {
         }
     }
 }
+function Clicked(Num) {
+    NowNum = Num
+    document.getElementById("Place").innerHTML = "<span>"+Place[Num]+"</span>"
+    PlaceP = ""
+    for ( i=0 ; i < PlaceIn[Num].length ; i++ ) {
+        PlaceP += '<div class="Links" onclick="InfoOp('+ String( PlaceIn[Num][i] ) +')"><span>'+MapList[PlaceIn[Num][i]][0]+'</span></div>'
+    }
+    document.getElementById("Link").innerHTML = '<div id="Place"><span>'+Place[Num]+'</span></div>'+PlaceP
+}
+function InfoOp(Num) {
+    if ( Num == 0 ) {
+        location.href = "../html/3_TTab.html"
+    } else {
+        document.getElementById("Info").classList.remove("HD")
+        document.getElementById("InfoBG").classList.remove("HD05")
+        document.getElementById("Info").classList.add("VV")
+        document.getElementById("InfoBG").classList.add("VV05")
+        document.getElementById("InfoGr").innerText = String( MapList[Num][0] );
+        InfoPlace = "";
+        for ( i=0 ; i<MapList[Num][1].length ; i++ ) {
+            InfoPlace += Place[MapList[Num][1][i]] + " "
+        }
+        document.getElementById("InfoPl").innerText = String( InfoPlace );
+        document.getElementById("InfoTi").innerText = String( MapList[Num][2] );
+        document.getElementById("InfoEx").innerHTML = String( MapList[Num][3] );
+    }
+}
+function InfoCl(){
+    document.getElementById("Info").classList.remove("VV")
+    document.getElementById("InfoBG").classList.remove("VV05")
+    document.getElementById("Info").classList.add("HD")
+    document.getElementById("InfoBG").classList.add("HD05")
+}
 (async function getData() {
-    const url = "../JavaS/database.json";
     try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`レスポンスステータス: ${response.status}`);
-      }
-      const json = await response.json();
-      MapList = json.MapList;
-      for ( i=1 ; i < MapList.length ; i++ ) {
+        const url = "../JavaS/database.json";
+        const response = await fetch(url);
+        const json = await response.json();
+        MapList = json.MapList;
+        Place = json.Place;
+      for ( i=0 ; i < MapList.length ; i++ ) {
         for ( j=0 ; j < MapList[i][1].length ; j++ ) {
-            if ( BlockP[MapList[i][1][j]] == "<span>" ) {
-                Blocks[MapList[i][1][j]].classList.add("Exist")
-            } else {
-                BlockP[MapList[i][1][j]] += "<br>"
+            if ( i != 0 ) {
+                if ( BlockP[MapList[i][1][j]] == "<span>" ) {
+                    Blocks[MapList[i][1][j]].classList.add("Exist")
+                } else {
+                    BlockP[MapList[i][1][j]] += "<br>"
+                }
+                BlockP[MapList[i][1][j]] += String( MapList[i][0] )
             }
-            BlockP[MapList[i][1][j]] += String( MapList[i][0] )
+            PlaceIn[MapList[i][1][j]].push( i )
         }
       }
       for ( i=0 ; i < Blocks.length ; i++ ) {
